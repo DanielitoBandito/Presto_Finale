49dc7f21afd87248c834179b06ef0a2bda307209
Antonio Santus <antonio.santus@icloud.com>
2024-10-08 19:19:50 +0200

    implementato fortify

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 // ROTTA HOMEPAGE
 Route::get('/', [HomeController::class,'index'])
 ->name('home.index');
+
+Route::get('/login' , [UserController::class, 'index'])
+->name('auth.login');
+
+

1350b9e1635034328250c139b8cd7690a8250cd7
Antonio Santus <antonio.santus@icloud.com>
2024-10-08 17:08:29 +0200

    components,view,route,navbar,buttonlogin,buttonRegister

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,47 @@
+<div style="width: 100%; height: 100px; background-color: rgb(0, 149, 182);  ">
+</div>
+<div class="navcustom">
+    <div class="navLeft">
+        <h1 style= "font-size:80px;">PRESTO</h1>
+    </div>
+    <div class="navRight">
+        <button class="btnLogin">ACCEDI</button>
+        <button class="btnRegistrer">REGISTRATI</button>
+    </div>
+</div>
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary mt-5 ">
+    <div class="container-fluid">
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Donna</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Uomo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Bambini</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Casa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Elettronica</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sport</a>
+                </li>
+
+
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Cerca</button>
+            </form>
+        </div>
+    </div>
+</nav>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+
 </head>
 <body>
-    <h1>ciao</h1>
-    <h2>Anto</h2>
+    <x-header />
+    {{$slot}}
+
+
+    @vite(['resources/css/app.css'])
+
+    
 </body>
 </html>

3a553d9567ef3c8321432432a42b15c2ff8a8483
Daniel <daniel.luongo5@gmail.com>
2024-10-09 17:59:26 +0200

    Migliorato il Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,25 @@
 <x-layout>
-    <form action="/login" method="post">
-        @csrf
+    <div class="mx-auto card p-4 shadow-lg" style="background-color: #0095b6; width: 100%; max-width: 500px; margin-top: 50px ">
+        <form action="/login" method="post" style="max-width: 500px; margin: 0 auto;" >
+            @csrf
 
-        <label for="email">Email</label>
-        <input class="form-control" type="email" placeholder="email" name="email" value="{{ old('email') }}">
-        @error('email')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
+            <label for="email">Email</label>
+            <input class="form-control" type="email" placeholder="email" name="email" value="{{ old('email') }}">
+            @error('email')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
-        <label for="password">Password</label>
-        <input class="form-control" type="password" placeholder="password" name="password">
-        @error('password')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
+            <label for="password">Password</label>
+            <input class="form-control" type="password" placeholder="password" name="password" style="margin-bottom: 10px;">
+            @error('password')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
-        <button type="submit">Accedi</button>
+            <button type="submit" class="btn btn-primary">Accedi</button>
 
 
-    </form>
+        </form>
+    </div>
+    
 
 </x-layout>

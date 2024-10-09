e63bdd2943376a23e87149af333310c7f208a32f
Daniel Luongo <Daniel.Luongo5@gmail.com>
2024-10-09 19:43:53 +0200

    Create le relazioni e migrazioni per l'articolo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Article;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -12,4 +14,9 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function articles():HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,18 @@
 <x-layout>
 
-
     <div class="mx-auto card p-4 shadow-lg" style="background-color: #0095b6; width: 100%; max-width: 500px; margin-top: 50px ">
         <form action="/login" method="post" style="max-width: 500px; margin: 0 auto;" >
             @csrf
-
-
 
             <label for="email">Email</label>
             <input class="form-control" type="email" placeholder="email" name="email" value="{{ old('email') }}">
             <x-form-errors />
 
-
             <label for="password">Password</label>
             <input class="form-control" type="password" placeholder="password" name="password" style="margin-bottom: 10px;">
             <x-form-errors />
 
-
             <button type="submit" class="btn btn-primary">Accedi</button>
-
 
         </form>
     </div>

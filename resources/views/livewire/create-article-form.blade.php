e41f648ca7330a6fdadd2a662957315ce9563223
Raffaele Tirone <tirone_raffy_93@hotmail.it>
2024-10-10 15:15:32 +0200

    form modificato

--- resources/views/livewire/create-article-form.blade.php
+++ resources/views/livewire/create-article-form.blade.php
@@ -1,17 +1,37 @@
-<div>
-    <form wire:submit="store" >
-        Titolo<input type="text" class="form-control" wire.model="title" id="title">
-        Prezzo<input type="text" class="form-control" wire.model="price" id="price">
-        Descrizione<input type="text" class="form-control" wire.model="description" id="description">
-        <select name="" id="category" wire:model="category" class="form-control mt-3">
-            <option value="">Seleziona una categoria</option>
-
+<form class="bg-body-tertiary shadow rounded p-5 my-5" wire:submit="store">
+    <div class="mb-3">
+        <label for="title" class="form-label">Titolo:</label>
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" wire:model.blur="title">
+        @error('title')
+            <p class="fst-italic text danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Descrizione:</label>
+        <textarea id="description" cols="30" rows="10" class="form-control @error('description') is-invalid @enderror" wire:model.blur="description"></textarea>
+        @error('description')
+            <p class="fst-italic text danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="price" class="form-label">Prezzo:</label>
+        <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" wire:model.blur='price'>
+        @error('price')
+            <p class="fst-italic text danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <select id="category" wire:model.blur="category" class="form-control @error('category') is-invalid @enderror">
+            <option label disabled>Seleziona una categoria:</option>
             @foreach ($categories as $category)
-                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                <option value="{{ $category->id }}">{{ $category->name}}</option>
             @endforeach
         </select>
-
-        <button type="submit" class="btn btn-primary mt-3">Crea</button>
-    </form>
-
-</div>
+        @error('category')
+            <p class="fst-italic text danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="d-flex justify-content-center">
+        <button type="submit" class="btn btn-dark">Crea</button>
+    </div>
+</form>

69042b52e435c0a17c4efa7b04fcb585af12b665
Daniel <daniel.luongo5@gmail.com>
2024-10-10 15:25:32 +0200

    Risolti 2 problemi (foot, pulsante)

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer style="background-color: rgb(0, 149, 182); text-align: center; position: absolute; bottom: 0.1; width: 100%">
+<footer style="background-color: rgb(0, 149, 182); text-align: center; position: absolute; bottom: 0; width: 100%">
     <h5>©2024 Presto.it | Privacy Policy</h5>
 </footer>

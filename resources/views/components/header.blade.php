948a6f3a1c5faf9e2c8a095bc3e28b3a30f018b4
Daniel Luongo <Daniel.Luongo5@gmail.com>
2024-10-10 21:25:23 +0200

    sistemato un po' il frontend

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,25 +9,12 @@
         <div class="collapse navbar-collapse"  id="navbarSupportedContent">
             <li class="nav-item"><a class="nav-link" href="{{ route('article.index')}}">Tutti</a></li>
 
-            {{-- <ul class="navbar-nav me-auto mb-2 mb-lg-0 " style="margin-left: 20px;">
-                <li class="nav-item"><a class="nav-link" href="#">Donna</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Uomo</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Bambini</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Casa</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Elettronica</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Sport</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Profumi</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Animali</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Musica</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Cosmetici</a></li>
-            </ul> --}}
-
-            <li class="nav-item dropdown">
+            <li class="nav-item dropdown mx-3">
                 <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorie</a>
-                <ul class="dropdown-menu">
+                <ul class="dropdown-menu bg-danger">
 
                     @foreach ($categories as $category)
-                        <li><a class="dropdown-item text-capitalize" href="{{ route('byCategory',['category' => $category])}}">{{ $category->name }}</a></li>
+                        <li class="bg-danger"><a class="nav-link text-capitalize" href="{{ route('byCategory',['category' => $category])}}">{{ $category->name }}</a></li>
                         @if (!$loop->last)
                             <hr class="dropdown-divider">
                         @endif
@@ -38,48 +25,50 @@
 
 
             
-            <form class="d-flex" role="search">
-                <input class="form-control" type="search" placeholder="Cerca" aria-label="Search">
-                <button class="btn btn-src" type="submit" style="margin-right: 30px;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 18">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                                </svg>      
-                </button>
-            </form>
+           
             
+            <div class="row position-absolute end-0 pb-0">
+                <form class="d-flex col-7 align-items-center justify-content-center" role="search">
+                    <input class="form-control" type="search" placeholder="Cerca" aria-label="Search">
+                    <button class="btn btn-src" type="submit" style="margin-right: 10px;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 18">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                                    </svg>      
+                    </button>
+                </form>
 
-            
-            <div>
-                @if (!auth()->check())
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <div class="col-5">
-                        <a href="/login" class="log-btn">ACCEDI</a>
+                <div class="col-5 p-0">
+                    @if (!auth()->check())
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <div class="col-5">
+                            <a href="/login" class="log-btn">ACCEDI</a>
+                        </div>
+                        <div class="col-6" >
+                            <a href="/register" class="btn-rg-hm">REGISTRATI</a>
+                        </div>
+                        
                     </div>
-                    <div class="col-6" >
-                        <a href="/register" class="btn-rg-hm">REGISTRATI</a>
-                    </div>
-                    
+    
+                    @else
+                        <div class="btn-group dropdown button m-1">
+                            <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Ciao, {{ Auth::user()->name }}
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item text-danger" href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout</a>
+                                    <form action="{{ route('logout') }}" method="post" class="d-none" id="form-logout">@csrf</form>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('article.create') }}">Crea</a>
+                                </li>
+                            </ul>
+                        </div>
+                    @endif
                 </div>
-
-
-
-                @else
-                    <div class="btn-group dropdown m-1">
-                        <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Ciao, {{ Auth::user()->name }}
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a class="dropdown-item text-danger" href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout</a>
-                                <form action="{{ route('logout') }}" method="post" class="d-none" id="form-logout">@csrf</form>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="{{ route('article.create') }}">Crea</a>
-                            </li>
-                        </ul>
-                    </div>
-                @endif
             </div>
+            
+           
         </div>
     </div>
 </nav>

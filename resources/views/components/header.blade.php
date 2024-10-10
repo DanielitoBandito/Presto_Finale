efe7b0b060624fc4f663ec9e38f98b070bd5bb6d
Daniel <daniel.luongo5@gmail.com>
2024-10-10 15:14:05 +0200

    Migliorato Accedi e Registrati

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -25,23 +25,32 @@
             <!-- Barra di ricerca -->
             <form class="d-flex" role="search">
                 <input class="form-control" type="search" placeholder="Cerca" aria-label="Search">
-                <button class="btn btn-src" type="submit" style="margin-right: 20px;">
+                <button class="btn btn-src" type="submit" style="margin-right: 30px;">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 18">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                                 </svg>      
                 </button>
             </form>
+            
 
             <!-- Pulsanti Accedi e Registrati / Menu Utente -->
             <div>
                 @if (!auth()->check())
-                    <div class="btn-group">
-                        <a href="/login" class="btn btn-success">ACCEDI</a>
-                        <a href="/register" class="btn btn-danger">REGISTRATI</a>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <div class="col-5">
+                        <a href="/login" class="log-btn">ACCEDI/</a>
                     </div>
+                    <div class="col-6" >
+                        <a href="/register" class="btn-rg-hm">REGISTRATI</a>
+                    </div>
+                    
+                </div>
+
+
+
                 @else
                     <div class="btn-group dropdown m-1">
-                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Ciao, {{ Auth::user()->name }}
                         </a>
                         <ul class="dropdown-menu">

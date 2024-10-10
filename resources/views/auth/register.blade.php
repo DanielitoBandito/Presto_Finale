901bfd861c156a51f06bd0273d906f0bacec09d9
Daniel <daniel.luongo5@gmail.com>
2024-10-10 16:46:31 +0200

    prova funzionante

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,27 +18,27 @@
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" name="email" id="email" class="form-input-rg" value="{{ old('email') }}">
+                            <input type="email" name="email" id="email" class="form-input-rg" value="{{ old('email') }}"  placeholder="Es. Mariorossi@gmail.com">
                             @error('email')
                                 <p class="fst-italic text danger">{{ $message }}</p>
                             @enderror
-                            <input type="email" name="email" id="email" class="form-input-rg" value="{{ old('email') }}" required placeholder="Es. Mariorossi@gmail.com">
+                            
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" name="password" id="password" class="form-input-rg">
+                            <input type="password" name="password" id="password" class="form-input-rg" placeholder="Inserisci una password">
                             @error('password')
                                 <p class="fst-italic text danger">{{ $message }}</p>
                             @enderror
-                            <input type="password" name="password" id="password" class="form-input-rg" required placeholder="Inserisci una password">
+                            
                         </div>
                         <div class="form-group">
                             <label for="password_confirmation">Conferma Password</label>
-                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-input-rg">
+                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-input-rg" placeholder="Conferma la tua password">
                             @error('password_confirmation')
                                 <p class="fst-italic text danger">{{ $message }}</p>
                             @enderror
-                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-input-rg" required placeholder="Conferma la tua password">
+                           
                         </div>
                         <div class="form-group mt-4 text-center">
                             <input type="submit" value="Registrati" class="btn btn-rg">

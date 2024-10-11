8bedc975eb3e4179025e0ce540922a43008fc198
Giulia Iannaccone <iannacconegiulia10@gmail.com>
2024-10-11 20:17:23 +0200

    prova di commit

--- resources/views/livewire/create-article-form.blade.php
+++ resources/views/livewire/create-article-form.blade.php
@@ -11,22 +11,17 @@
         <label for="title" class="article-add">Titolo:</label>
         <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" wire:model.blur="title" placeholder="Inserisci il titolo">
         @error('title')
-<<<<<<< HEAD
             <p class="error-message">{{ $message }}</p>
-=======
             <p class="fst-italic text danger">{{ $message }}</p>
->>>>>>> a4c5b6f4c62ee98d2eb27e32ab987649b2d073a4
         @enderror
     </div>
     <div class="mb-3">
         <label for="description" class="article-add">Descrizione:</label>
         <textarea id="description" cols="30" rows="10" class="form-control @error('description') is-invalid @enderror" wire:model.blur="description" placeholder="Inserisci la descrizione"></textarea>
         @error('description')
-<<<<<<< HEAD
             <p class="error-message">{{ $message }}</p>
 =======
             <p class="fst-italic text danger">{{ $message }}</p>
->>>>>>> a4c5b6f4c62ee98d2eb27e32ab987649b2d073a4
         @enderror
     </div>
     <div class="mb-3">
@@ -41,24 +36,19 @@
         <label for="category" class="article-add">Categoria:</label>
         <select id="category" wire:model.blur="category" class="fs-5 form-control @error('category') is-invalid @enderror">
             <option  value="" label >Seleziona una categoria:</option>
-=======
             <p class="fst-italic text danger">{{ $message }}</p>
         @enderror
     </div>
     <div class="mb-3">
         <select id="category" wire:model.blur="category" class="form-control @error('category') is-invalid @enderror">
             <option value="" label>Seleziona una categoria:</option>
->>>>>>> a4c5b6f4c62ee98d2eb27e32ab987649b2d073a4
             @foreach ($categories as $category)
                 <option value="{{ $category->id }}">{{ $category->name}}</option>
             @endforeach
         </select>
         @error('category')
-<<<<<<< HEAD
             <p class="error-message">{{ $message }}</p>
-=======
             <p class="fst-italic text danger">{{ $message }}</p>
->>>>>>> a4c5b6f4c62ee98d2eb27e32ab987649b2d073a4
         @enderror
     </div>
     <div class="d-flex justify-content-center">

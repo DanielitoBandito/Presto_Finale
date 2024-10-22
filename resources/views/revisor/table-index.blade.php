b03f8c5030b4e63831532fea2efeffb0e20e3e34
Raffaele Tirone <tirone_raffy_93@hotmail.it>
2024-10-22 12:12:55 +0200

    logica revisore completata dopo 3852 settimane

--- resources/views/revisor/table-index.blade.php
+++ resources/views/revisor/table-index.blade.php
@@ -9,23 +9,26 @@
         </div>
         <div class="row height-custom justify-content-center align item-center py-5">
             @forelse ($articles_checked as $article)
-                <div class="col-12 col-md-4">
-                    <div class="article-card">
-                        <img src="/images/logo-color-edited.png" alt="Nome Articolo" class="article-image">
-                        <div class="article-details">
-                            <h2 class="article-name">{{ $article->title }}</h2>
-                            <p class="article-description text-truncate">{{ $article->description }}</p>
-                            <p class="article-price">€ {{ $article->price }}</p>
-                            <div class="article-buttons">
-                                @if ($article->user_id !== auth()->id())
-                                    <a href="{{route('revisor.undoLastAction', ['article' => $article->id])}}" class="detail-button">Annulla Revisione</a>                           
+                @if ($article->user_id !== auth()->id())
+                    <div class="col-12 col-md-4">
+                        <div class="article-card">
+                            <img src="/images/logo-color-edited.png" alt="Nome Articolo" class="article-image">
+                            <div class="article-details">
+                                <h2 class="article-name">{{ $article->title }}</h2>
+                                <p class="article-description text-truncate">{{ $article->description }}</p>
+                                <p class="article-price">€ {{ $article->price }}</p>
+                                @if ($article->is_accepted == true)
+                                    <p class="text-success">L'articolo in precedenza revisionato è stato accettato</p>
                                 @else
-                                    <p class="bg-danger text-white">La revisione del tuo articolo può essere annullata solo da un altro revisore</p>
+                                    <p class="text-danger">L'articolo in precedenza revisionato è stato rifiutato</p>
                                 @endif
+                                <div class="article-buttons">
+                                    <a href="{{route('revisor.undoLastAction', ['article' => $article->id])}}" class="detail-button">Annulla Revisione</a>                           
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endif
             @empty
                 <div class="col-12 col-md-8">
                     <h3 class="text-center">

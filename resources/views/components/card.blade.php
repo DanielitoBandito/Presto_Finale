27ad10092d3169b6a6381c1b660e53f85e7a6a11
Raffaele Tirone <tirone_raffy_93@hotmail.it>
2024-10-22 15:12:47 +0200

    card immagine modificata

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 <div class="article-card">
     <img src="{{ $article->images->count() > 0 ? Storage::url($article->images->first()->path) : '/images/logo-color-edited.png' }}"
-        alt="Immagine dell'articolo {{ $article->title }}}" class="card-img-top">
+        alt="Immagine dell'articolo {{ $article->title }}" class="card-img-top img-fluid" style="height: 200px; object-fit: cover;">
     <div class="article-details">
         <h2 class="article-name">{{ $article->title }}</h2>
         <p class="article-description text-truncate">{{ $article->description }}</p>

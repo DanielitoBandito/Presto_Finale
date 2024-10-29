137bcd948a31669e499340c14c4e0af0b26bf4fc
Daniel <daniel.luongo5@gmail.com>
2024-10-29 14:27:45 +0100

    Rivista situazione Revisor, Articoli da revisionare

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -47,11 +47,11 @@
         <div class="dropdown">
             <button class=" dropdown-toggle-lang" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
-                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484q-.121.12-.242.234c-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
-                  </svg>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
-                    <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"/>
-                  </svg>
+                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484q-.121.12-.242.234c-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+                    <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659" />
+                </svg>
             </button>
             <ul class="dropdown-menu dropdown-lang" aria-labelledby="dropdownMenuButton">
                 <li><x-_locale lang="it" /></li>
@@ -77,31 +77,31 @@
 
             </div>
             @else
-                <div class="btn-group dropdown button m-1">
-                    <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        {{ __('ui.Ciao')}}, {{ Auth::user()->name }}
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a class="dropdown-item" href="{{ route('article.create') }}">{{ __('ui.Crea Articolo') }}</a>
-                        </li>
-                        @if (Auth::user()->is_revisor == true)
-                            <li>
-                                <a class="dropdown-item" href="{{ route('revisor.index') }}">{{ __('ui.Revisore') }}
-                                    <span class="badge rounded-pill bg-danger">
-                                        {{ \App\Models\Article::toBeRevisedCount() }}</span></a>
-                            </li>
-                        @endif
-                        <li>
-                            <a class="dropdown-item text-danger" href="#"
-                                onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">{{ __('ui.Logout') }}</a>
-                            <form action="{{ route('logout') }}" method="post" class="d-none" id="form-logout">
-                                @csrf
-                            </form>
-                        </li>
-                    </ul>
-                </div>
+            <div class="btn-group dropdown button m-1">
+                <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    {{ __('ui.Ciao')}}, {{ Auth::user()->name }}
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="dropdown-item" href="{{ route('article.create') }}">{{ __('ui.Crea Articolo') }}</a>
+                    </li>
+                    @if (Auth::user()->is_revisor == true)
+                    <li>
+                        <a class="dropdown-item" href="{{ route('revisor.index') }}">{{ __('ui.Revisore') }}
+                            <span class="badge rounded-pill bg-danger">
+                                {{ \App\Models\Article::toBeRevisedCount() }}</span></a>
+                    </li>
+                    @endif
+                    <li>
+                        <a class="dropdown-item text-danger" href="#"
+                            onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">{{ __('ui.Logout') }}</a>
+                        <form action="{{ route('logout') }}" method="post" class="d-none" id="form-logout">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </div>
             @endif
         </div>
 
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -3,101 +3,90 @@
     <h1 style="text-align: center">{{__('ui.Dashboard')}}:</h1>
 
     <div class=" row pt-3 mb-auto mx-auto w-100 col-12">
-        <div class="col-md-3">
-            <aside class="aside bg-body-secondary " style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;">
-                <div class="col-md-3 w-100">
-                    <div class="art-rev">
-                        <a href="{{ route('revisor.index') }}" class="fs-4 pb-2" style="text-decoration: none; color: black;">{{__('ui.Dashboard')}} </a>
-                    </div>
+        <x-aside_rev></x->
 
-                    <div class=" art-rev">
-                        <a href="{{ route('table.index') }}" class="fs-4 pb-2"
-                            style="text-decoration: none; color: black;">{{__('ui.Articoli revisionati')}}</a>
-                    </div>
-                </div>
-            </aside>
-        </div>
+            <div class="col-md-9">
+                <div class="bg-body-secondary rounded p-4" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;">
 
-        <div class="col-md-9">
-            <div class="bg-body-secondary rounded p-4" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;">
-
-                @if (!$article_to_check)
-                <div class="row justify-content-center align-items-center text-center height-custom">
-                    <div class="col-12">
-                        <h1 class="fst-italic fs-2">{{ __('ui.Nessun articolo da revisionare') }}</h1>
-                        <a href="{{ route('home.index') }}" class="mt-5 btn btn-success">{{ __("ui.Torna all'homepage") }}</a>
-                    </div>
-                </div>
-                @else
-                @if (session()->has('message'))
-                <div class="row justify-content-center">
-                    <div class="col-12 text-center">
-                        <div class="alert alert-{{ session('status') }} fade show position-fixed top-3 start-50 translate-middle-x" role="alert" style="z-index: 1050;">
-                            {{ session('message') }}
+                    @if (!$article_to_check)
+                    <div class="row justify-content-center align-items-center text-center height-custom">
+                        <div class="col-12">
+                            <h1 class="fst-italic fs-2">{{ __('ui.Nessun articolo da revisionare') }}</h1>
+                            <a href="{{ route('home.index') }}" class="mt-5 btn btn-success">{{ __("ui.Torna all'homepage") }}</a>
                         </div>
                     </div>
-                </div>
-                @endif
-
-                <!-- Blocco informazioni articolo -->
-                <div class="row mb-4">
-                    <div class="col-md-8">
-                        <h2 class="text-truncate">{{ $article_to_check->title }}</h2>
-                        <h3>{{ __('ui.Autore') }}: {{ $article_to_check->user->name }}</h3>
-                        <h4>{{ $article_to_check->price }} €</h4>
-                        <h5 class="fst-italic text-muted">{{ $article_to_check->category->name }}</h5>
-                        <p class="h6 text-truncate">{{ $article_to_check->description }}</p>
-                    </div>
-
-                    <div class="col-md-4 d-flex flex-column justify-content-end align-items-center gap-3">
-                        <a href="{{ route('article.show', ['article' => $article_to_check->id]) }}" class="btn btn-info">Dettaglio</a>
-
-                        <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button class="btn btn-danger fw-bold">{{ __('ui.Rifiuta') }}</button>
-                        </form>
-                        <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button class="btn btn-success fw-bold">{{ __('ui.Accetta') }}</button>
-                        </form>
-                    </div>
-                </div>
-                <!-- Fine blocco informazioni articolo -->
-
-                <div class="row pt-5">
-                    @foreach ($article_to_check->images as $key => $image)
-                    <div class="col-md-3 col-lg-3 mb-4">
-                        <div class="card shadow-sm">
-                            <img src="{{ $image->getUrl() }}" class="card-img-top rounded" alt="Immagine {{ $key + 1 }} dell'articolo '{{ $article_to_check->title }}'">
-                            <div class="card-body" style="text-align: center">
-                                <h5 class="card-title">Labels</h5>
-                                @if ($image->labels)
-                                <p>
-                                    @foreach ($image->labels as $label)
-                                    #{{ $label }},
-                                    @endforeach
-                                </p>
-                                @else
-                                <p class="fst-italic text-muted">No labels</p>
-                                @endif
-
-                                <h5 class="mt-3">Ratings</h5>
-                                @foreach (['adult', 'violence', 'spoof', 'racy', 'medical'] as $rating)
-                                <div class="d-flex align-items-center my-2">
-                                    <span class="badge bg-{{ $image->$rating ? 'danger' : 'secondary' }} mx-2"></span>
-                                    <span>{{ $rating }}</span>
-                                </div>
-                                @endforeach
+                    @else
+                    @if (session()->has('message'))
+                    <div class="row justify-content-center">
+                        <div class="col-12 text-center">
+                            <div class="alert alert-{{ session('status') }} fade show position-fixed top-3 start-50 translate-middle-x" role="alert" style="z-index: 1050;">
+                                {{ session('message') }}
                             </div>
                         </div>
                     </div>
-                    @endforeach
+                    @endif
+
+                    <!-- Blocco informazioni articolo -->
+                    <div class="row mb-4">
+                        <div class="col-md-8">
+                            <h2 class="text-truncate">{{ $article_to_check->title }}</h2>
+                            <h3>{{ __('ui.Autore') }}: {{ $article_to_check->user->name }}</h3>
+                            <h4>{{ $article_to_check->price }} €</h4>
+                            <h5 class="fst-italic text-muted">{{ $article_to_check->category->name }}</h5>
+                            <p class="h6 text-truncate">{{ $article_to_check->description }}</p>
+                        </div>
+
+                        <div class="col-md-4 d-flex flex-column justify-content-end align-items-center gap-3">
+                            <div class="d-flex gap-3">
+                                <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="POST">
+                                    @csrf
+                                    @method('PATCH')
+                                    <button class="btn btn-danger fw-bold" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;">{{ __('ui.Rifiuta') }}</button>
+                                </form>
+                                <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="POST">
+                                    @csrf
+                                    @method('PATCH')
+                                    <button class="btn btn-success fw-bold" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;">{{ __('ui.Accetta') }}</button>
+                                </form>
+                            </div>
+
+                            <a href="{{ route('article.show', ['article' => $article_to_check->id]) }}" class="btn btn-info w-100" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;">Dettaglio</a>
+                        </div>
+                    </div>
+                    <!-- Fine blocco informazioni articolo -->
+
+                    <div class="row pt-5">
+                        @foreach ($article_to_check->images as $key => $image)
+                        <div class="col-md-3 col-lg-3 mb-4">
+                            <div class="card shadow-sm">
+                                <img src="{{ $image->getUrl() }}" class="card-img-top rounded" alt="Immagine {{ $key + 1 }} dell'articolo '{{ $article_to_check->title }}'">
+                                <div class="card-body" style="text-align: center">
+                                    <h5 class="card-title">Labels</h5>
+                                    @if ($image->labels)
+                                    <p>
+                                        @foreach ($image->labels as $label)
+                                        #{{ $label }},
+                                        @endforeach
+                                    </p>
+                                    @else
+                                    <p class="fst-italic text-muted">No labels</p>
+                                    @endif
+
+                                    <h5 class="mt-3">Ratings</h5>
+                                    @foreach (['adult', 'violence', 'spoof', 'racy', 'medical'] as $rating)
+                                    <div class="d-flex align-items-center my-2">
+                                        <span class="badge bg-{{ $image->$rating ? 'danger' : 'secondary' }} mx-2"></span>
+                                        <span>{{ $rating }}</span>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    @endif
                 </div>
-                @endif
             </div>
-        </div>
 
 
 
